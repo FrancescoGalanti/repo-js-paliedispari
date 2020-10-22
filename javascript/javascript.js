d451cdafa9c4b3465de6952e0b2780581da54741
FrancescoGalanti <galanti_francesco>
2020-10-22 14:32:11 +0200

    secondo esercizio prima funzione fatta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,29 +6,50 @@
 
  //prima variabile //
 
- var parola = prompt("inserisci una parola")
+//  var parola = prompt("inserisci una parola")
+//
+// var parolareverse =  reverse(parola)
+//
+// console.log(parolareverse)
+//
+// if(parola == parolareverse){
+//   console.log("Il giocatore ha vinto");
+// } else{
+//   console.log("il giocatore non ha vinto");
+// }
+//
+//
+//
+//
+//
+//
+// /*
+// ** Creazione funzione reverse Un solo parametro
+// **/
+//
+// function reverse(word){
+//    var revers = word.split("").reverse().join();
+//
+//    return revers;
+// }
 
-var parolareverse =  reverse(parola)
 
-console.log(parolareverse)
+// secondo esercizio //
 
-if(parola == parolareverse){
-  console.log("Il giocatore ha vinto");
-} else{
-  console.log("il giocatore non ha vinto");
-}
+// prima variabile //
 
+var utenteparidispari = prompt("Scegli pari o dispari");
+var numeroutente = parseInt(prompt("Scegli un numero da 1 a 5"));
+var numerocomputer = random();
 
-
-
+console.log(numerocomputer)
 
 
 /*
-** Creazione funzione reverse Un solo parametro
-**/
+** Creazione funzione numero random da 1 a 5
+*/
+function random(){
+  var rand = Math.floor(Math.random()* 5) + 1;
 
-function reverse(word){
-   var revers = word.split("").reverse().join();
-
-   return revers;
+  return rand;
 }
