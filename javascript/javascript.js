01e7dd63fa8f43c791d81e5a63318f2e93cee185
FrancescoGalanti <galanti_francesco>
2020-10-22 13:55:42 +0200

    primo esercizio fatto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,47 +4,31 @@
 
 
 
-// // prima variabile //
-//  var email = ["email@francesco.it", "email@dario.it", "email@laura.it"];
-//
-//  // seconda variabile //
-//
-//  var emailuser = prompt("Inserisci la tua email").toLowerCase();
-//
-//  // controllo//
-//
-//  var userfound = false;
-//
-//
-//  for( var i = 0;  i < email.length; i++ ){
-//
-//     var item = email[i];
-//
-//     if( item === emailuser) {
-//         userfound = true;
-//     }
-//
-//  }
-//
-//  if(userfound == true) {
-//    console.log("Utente trovato, procedi con l\"accesso");
-//  } else(
-//     console.log("errore di autenticazione")
-//  )
+ //prima variabile //
 
-// // secondo esercizio //
-//
-// // prima variabile //
-// var numerogiocatore = Math.floor((Math.random() * 6) + 1);
-//
-// // console.log(numerogiocatore) //
-//
-// var numerocomputer = Math.floor((Math.random() * 6) + 1);
-//
-// // output //
-//
-// if(numerogiocatore > numerocomputer) {
-//   console.log("IL GIOCATORE VINCE")
-// } else{
-//   console.log("IL COMPUTER VINCE")
-// }
+ var parola = prompt("inserisci una parola")
+
+var parolareverse =  reverse(parola)
+
+console.log(parolareverse)
+
+if(parola == parolareverse){
+  console.log("Il giocatore ha vinto");
+} else{
+  console.log("il giocatore non ha vinto");
+}
+
+
+
+
+
+
+/*
+** Creazione funzione reverse Un solo parametro
+**/
+
+function reverse(word){
+   var revers = word.split("").reverse().join();
+
+   return revers;
+}
